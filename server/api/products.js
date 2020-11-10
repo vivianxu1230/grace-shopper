174726177109252307292d035b44960079ddd295
Madison Kneller <knellermadison@gmail.com>
2020-11-10 16:05:09 -0600

    Updated navbar functionality and added temporary image

--- server/api/products.js
+++ server/api/products.js
@@ -4,14 +4,25 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await Product.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+    const product = await Product.findAll()
+    // explicitly select only the id and email fields - even though
+    // users' passwords are encrypted, it won't help if we just
+    // send everything to anyone who asks!
+    res.json(product)
   } catch (err) {
     next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        id: req.params.productId
+      }
+    })
+    res.json(product)
+  } catch (error) {
+    next(error)
   }
 })
