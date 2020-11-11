0240bc8062ebdf174c3173c1991a81bc201eccbb
Vivian <vivianxu97@protonmail.com>
2020-11-11 09:52:47 -0600

    adding cart route and icon

---
+++ client/components/cart.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {logout} from '../store'
+
+const Cart = ({isLoggedIn}) => (
+  <div>
+    {/* {list of cart items here} */}
+    {isLoggedIn ? (
+      <div>
+        <button type="button">Checkout</button>
+      </div>
+    ) : (
+      <div>
+        <p>Log in or sign up to check out</p>
+      </div>
+    )}
+  </div>
+)
+
+/**
+ * CONTAINER
+ */
+const mapState = (state) => {
+  console.log(state)
+  return {
+    isLoggedIn: !!state.user.id,
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleClick() {
+      dispatch(logout())
+    },
+  }
+}
+
+export default connect(mapState, mapDispatch)(Cart)
+
+/**
+ * PROP TYPES
+ */
+// Navbar.propTypes = {
+//   handleClick: PropTypes.func.isRequired,
+//   isLoggedIn: PropTypes.bool.isRequired,
+// }
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, allProducts, Home} from './components'
+import {Login, Signup, UserHome, allProducts, Home, Cart} from './components'
 import {me} from './store'
 
 /**
@@ -20,6 +20,7 @@ class Routes extends Component {
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route exact path="/" component={Home} />
+        <Route path="/cart" component={Cart} />
         {/* <Route path="/login" component={Login} /> */}
         // <Route path="/signup" component={Signup} />
         <Route path="/allproducts" component={allProducts} />
@@ -39,19 +40,19 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
   }
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
       dispatch(me())
-    }
+    },
   }
 }
 
@@ -64,5 +65,5 @@ export default withRouter(connect(mapState, mapDispatch)(Routes))
  */
 Routes.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
 }
