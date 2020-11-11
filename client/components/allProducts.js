81582eb2262e4ec1a70842513fc34e15dd43182b
Gianni LaTange <glatange@gmail.com>
2020-11-11 09:17:42 -0800

    products component and reducer completed

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,36 +1,42 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { fetchProducts } from '../store/products'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../store/products'
 
 export class allProducts extends React.Component {
-   componentDidMount() {
-       this.props.fetchProducts();
-   }
-   
-   render () {
-        const { products } = this.props
-        return (
-            <div>
-                {products.map(product => {
-                    return (
-                        <div className="products" key={product.id}>
-                           <a className="link" href={`/products/${product.id}`}> {product.name}</a>
-                           <img style={{width: '500px', height: '500px'}} src={product.imageUrl} />
-                           <p>{product.price}</p>
-                        </div>
-                    )
-                })}
+  componentDidMount() {
+    this.props.fetchProducts()
+  }
+
+  render() {
+    const {products} = this.props
+    return (
+      <div>
+        {products.map(product => {
+          return (
+            <div className="products" key={product.id}>
+              <a className="link" href={`/products/${product.id}`}>
+                {' '}
+                {product.name}
+              </a>
+              <img
+                style={{width: '500px', height: '500px'}}
+                src={product.imageUrl}
+              />
+              <p>{product.price}</p>
             </div>
-        )
-    }
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-const mapState = (state) => ({
-    proudcts: state.products
+const mapState = state => ({
+  proudcts: state.products
 })
 
-const mapDispatch = (dispatch) => ({
-    fetchProducts: () => dispatch(fetchProducts())
+const mapDispatch = dispatch => ({
+  fetchProducts: () => dispatch(fetchProducts())
 })
 
 export default connect(mapState, mapDispatch)(allProducts)
--- client/store/products.js
+++ client/store/products.js
@@ -1,37 +1,37 @@
 import axios from 'axios'
 
-// Action Types 
+// Action Types
 const ALL_PRODUCTS = 'ALL_PRODUCTS'
 
 // Action Creators
-export const allProducts = (products) => ({
-    type: ALL_PRODUCTS,
-    products
+export const allProducts = products => ({
+  type: ALL_PRODUCTS,
+  products
 })
 
 // Fetch Thunk Creator
 export const fetchProducts = () => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get('/api/products');
-            dispatch(allProducts(data))
-        } catch (error) {
-            console.log(error)
-        }
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/products')
+      dispatch(allProducts(data))
+    } catch (error) {
+      console.log(error)
     }
+  }
 }
 
-// Initial State 
+// Initial State
 const initialState = []
 
 const productsReducer = (state = initialState, action) => {
-    switch (action.type){
-        case ALL_PRODUCTS:
-            return action.products
-    
+  switch (action.type) {
+    case ALL_PRODUCTS:
+      return action.products
+
     default:
-        return state
-    }
+      return state
+  }
 }
 
 export default productsReducer
