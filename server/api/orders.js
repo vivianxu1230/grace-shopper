32711dd3d2e149a15ed99fcb25164eac35c0a8ad
Gianni LaTange <glatange@gmail.com>
2020-11-17 17:08:40 -0500

    route protection middleware

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,8 +1,24 @@
-const {adminsOnly, adminsAndusers} = require('./index')
 const router = require('express').Router()
 const {Order} = require('../db/models')
 module.exports = router
 
+const adminsOnly = (req,res,next) => {
+  if (!req.user.isAdmin) {
+    const err = new Error('No access.')
+    err.status = 401
+    return next(err)
+  }
+  next()
+}
+
+const adminsAndusers = (req,res,next) => {
+  if (!req.user.isAdmin || req.user.userId !== req.params.userId) {
+    const err = new Error('No access.')
+    err.status = 401
+    return next(err)
+  }
+  next()
+}
 router.get('/', async (req, res, next) => {
   try {
     const orders = await Order.findAll()
